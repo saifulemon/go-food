60cc285d285df8b304ac49c4215d9bf15ac5801b
saifulemon <463saiful0@gmail.com>
2022-12-24 19:18:53 +0600

    footer component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Shared/Footer/Footer';
+import Navbar from './components/Shared/Navbar/Navbar';
 import Home from './pages/Home/Home';
 
 function App() {
   return (
+
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
